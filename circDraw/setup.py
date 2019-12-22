3763e22f2216638e87a48df9f83ed687fd6b4c93
Tianqin Li <jacklitianqin@gmail.com>
2019-12-21 22:26:31 -0500

    add test data

--- circDraw/setup.py
+++ circDraw/setup.py
@@ -5,14 +5,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="circDraw",
-    version="0.0.1",
+    version="0.1.0",
     author="Yimin Zheng, Tianqin Li",
-    author_email="zym.zym1220@gmail.com",
-    description="A python package for circDraw visualization",
+    author_email="zym.zym1220@gmail.com, jacklitianqin@gmail.com",
+    description="A python package for circDraw visualization and server command line interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mr-Milk/circDraw-py",
-    packages=setuptools.find_packages(),
+    packages=['circDraw'],
+    entry_points = {
+        'console_scripts':[
+            'circDraw = circDraw.__main__:main'
+            ]
+        },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
